012ef16214206f2524ab63211ac7fa940e8d20f4
dev-ruby <dldmsgh0612@gmail.com>
2021-01-01 00:10:14 +0900

    Update run.py

--- src/run.py
+++ src/run.py
@@ -8,13 +8,14 @@ class Bot(commands.Bot):
     def __init__(self):
         super().__init__(irc_token=os.environ["hunter_irc_token"], client_id=os.environ["hunter_client_id"], nick="thebotofhunter", prefix='!', initial_channels=["#zsv1457"])
 
-    # Events don't need decorators when subclassed
     async def event_ready(self):
-        print(f'Ready | {self.nick}')
-
+        print('Ready')
+        ch = self.get_channel("zsv1457")
+        await ch.send("")
     async def event_message(self, message):
         print(message.content)
         await self.handle_commands(message)
+    
     @commands.command(name='test')
     async def test(self, ctx):
         pass
