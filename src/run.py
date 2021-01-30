5197ec27d808c6a90f89f4234936378a655714e1
dev-ruby <dldmsgh0612@gmail.com>
2021-01-30 22:03:15 +0900

    ㅁㄴㅇㄹ

--- src/run.py
+++ src/run.py
@@ -1,6 +1,7 @@
 from twitchio.ext import commands
 import os
 import asyncio
+import sys
 import json
 import random
 
@@ -16,29 +17,58 @@ class Bot(commands.Bot):
         )
 
     async def event_ready(self):
-        print("Ready")
         ch = self.get_channel("zsv1457")
-        await ch.send("봇 가동 시작")
         while True:
             await asyncio.sleep(10)
             save()
 
     async def event_message(self, message):
-        if message.author.id == 1239010238 or message.author.id == 0:
+        if message.author.id == 0:
             return
-        data[str(message.author.id)] = (
-            data[str(message.author.id)]
-            + (100 + len(message.content) * 3 + random.randint(20, 100))
-            if data.get(str(message.author.id)) is not None
-            else (100 + len(message.content) * 3 + random.randint(20, 100))
-        )
-
-        if message.content in ["!point", "!포인트"]:
+        if message.author.id not in [485369763, 520690385]:
+            data[str(message.author.id)] = {
+                "point": (
+                        (
+                            data[str(message.author.id)]["point"]
+                            if data.get(str(message.author.id)) is not None
+                            else 0
+                        )
+                        + (100 + len(message.content) * 3 + random.randint(20, 100))
+                ),
+                "name": message.author.name,
+            }
+        else:
+            data["485369763"] = {"point": 0, "name": "헌터팬티"}
+            data["520690385"] = {"point": 0, "name": "세카"}
+        if message.content == "!restart":
+            if message.author.id == 485369763:
+                await message.channel.send("restarting..")
+                save()
+                os.system("python ./src/run.py")
+                sys.exit()
+            else:
+                await message.channel.send("권한이 없습니다")
+        if message.content in ["!point", "!포인트", "!points"]:
             await message.channel.send(point(message.author))
-
+        if message.content in ["!ranking", "!랭킹"]:
+            users = list()
+            ranking = list()
+            for i in data:
+                users.append(data[i])
+            for i in range(len(users)):
+                x = users[0]
+                for i in range(len(users) - 1):
+                    if x["point"] < users[i + 1]["point"]:
+                        x = users[i + 1]
+                users.remove(x)
+                ranking.append(x)
+            for i in range(3):
+                await message.channel.send(
+                    f"{i + 1}등 : {ranking[i]['name']}, {ranking[i]['point']} points"
+                )
 
 def point(user):
-    return f"{user.name} 님의 잔여 포인트는 {data[str(user.id)] if data.get(str(user.id)) is not None else 0} 점 입니다."
+    return f"{user.name} 님의 잔여 포인트는 {data[str(user.id)]['point'] if data.get(str(user.id))['point'] is not None else 0} 점 입니다."
 
 
 data = dict
@@ -47,7 +77,6 @@ if not os.path.exists("./points.json"):
         fp.write("{}")
 with open("./points.json", "r") as fp:
     data = json.load(fp)
-print(data)
 
 
 def save():
@@ -55,6 +84,7 @@ def save():
         json.dump(data, fp)
 
 
+os.system('cls')
 bot = Bot()
 bot.run()
 
